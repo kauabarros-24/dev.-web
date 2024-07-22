007e80f26412c5a1267c10b3a54c82aa7d96169c
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-07-22 16:42:32 -0300

    Diretivas

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,7 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-
-const contador = ref(0);
-
-function incrementarContador() {
-  contador.value++
-}
-function decrementarContador() {
-  if (contador.value == 0) 
-    return
-  contador.value--
-}
-function reiniciar() {
-  if (contador.value != 0) {
-    contador.value = 0
-  }
-}
-
-onMounted(() => {
-  console.log(`O valor do contador é ${contador.value}`)
-})
+    const url = 'https://vuejs.org/';
 </script>
 
 <template>
-  <button @click="incrementarContador">Incremetar</button>
-  <button @click="decrementarContador">decrementar</button>
-  <button @click="reiniciar">Reiniciar</button>
-
-  <p>O valor do conteúdo é: {{ contador }}</p>
+    <a v-bind:href="url">Site do vueJS</a>
 </template>
-
-<style scoped>
-button {
-  font-weight: bold;
-}
-</style>
