b1b536ea06f9c78156913b546ea8a5a8bc250318
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-07-22 16:24:04 -0300

    Contador com api de opções

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
-<script>
-export default {
-  data() {
-    return {
-      contador: 0
-    }
-  },
-  methods: {
-    incrementarContador() { 
-      this.contador++
-     },
-     decrementarContador() {
-      if (this.contador > 0) 
-        this.contador--
-     }
-  },
-  mounted() {
-    console.log(`O valor inicial do contador é ${this.contador}`)
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const contador = ref(0);
+
+function incrementarContador() {
+  contador.value++
+}
+function decrementarContador() {
+  if (contador.value == 0) 
+    return
+  contador.value--
+}
+function reiniciar() {
+  if (contador.value != 0) {
+    contador.value = 0
   }
 }
 
+onMounted(() => {
+  console.log(`O valor do contador é ${contador.value}`)
+})
 </script>
 
 <template>
   <button @click="incrementarContador">Incremetar</button>
   <button @click="decrementarContador">decrementar</button>
+  <button @click="reiniciar">Reiniciar</button>
+
   <p>O valor do conteúdo é: {{ contador }}</p>
 </template>
 
